1b7a35b51eb11014c5ce7e7589c86f9a37abf552
pcaracal <peter.schreivogel@stud.kbw.ch>
2023-05-12 08:41:20 +0200

    about site done for now

--- versicherungsrechner/src/sites/About.js
+++ versicherungsrechner/src/sites/About.js
@@ -4,6 +4,14 @@ const About = () => {
         <div className="About">
             <main className="container">
                 <h1>About</h1>
+                <p>
+                    Versicherungsrechner by <strong>Peter Schreivogel</strong><br />
+                    Berufbildungsschule Winterthur BBW
+                </p>
+                <h2>Kontakt</h2>
+                <p>
+                    peter.schreivogel@lernende.bbw.ch
+                </p>
             </main>
         </div>
     );
