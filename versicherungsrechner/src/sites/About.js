995dbd1a2d558f6b072ae0c2cefed6e700a1abba
pcaracal <peter.schreivogel@stud.kbw.ch>
2023-05-12 10:47:21 +0200

    More information in about page

--- versicherungsrechner/src/sites/About.js
+++ versicherungsrechner/src/sites/About.js
@@ -8,6 +8,12 @@ const About = () => {
                     Versicherungsrechner by <strong>Peter Schreivogel</strong><br />
                     Berufbildungsschule Winterthur BBW
                 </p>
+
+                <p>
+                    Dies ist eine kleine Website mit der Sie die Schadenzahlung der Hausratsversicherung berechnen können.<br />
+                    Die Website wurde erstellt gemäss einer vagen Aufgabenstellung von der BBW in zusammenarbeit mit der KBW.
+                </p>
+
                 <h2>Kontakt</h2>
                 <p>
                     peter.schreivogel@lernende.bbw.ch
